5c45e86b4112a50cb0e0564343bf5efd42ca7bc9
Chase Million <chase@millionconcepts.com>
2018-06-15 16:02:29 -0400

    Adding a tool that will read PDS4 data into a Pandas DataFrame or write it to a .csv file.

--- src/pds.py
+++ src/pds.py
@@ -1,9 +1,15 @@
 import ast
-import numpy as np
 import struct
+import pds4_tools
+import numpy as np
+import pandas as pd
+import os
+from collections import OrderedDict
 
 def read_label(labelname):
-    """ Read a freestanding PDS Data Label (.LBL) into a dict()."""
+    """ Read a freestanding PDS Data Label (.LBL) into a dict().
+    Might not work properly!
+    """
     if labelname[-4:] in ['.img','.IMG']:
         print 'Attempting to read label from image file.'
     if not labelname[-4:] in ['.lbl','.LBL']:
@@ -62,6 +68,7 @@ def read_label(labelname):
     return label
 
 def read_image(imagename,labelname=None):
+    """ This doesn't work properly... don't use it."""
     if not labelname:
         label = read_label(imagename)
     else:
@@ -86,3 +93,35 @@ def read_image(imagename,labelname=None):
         #masked = [label['IMAGE']['SAMPLE_BIT_MASK'] & u for u in unmasked]
         return np.array(unmasked).reshape(
                     label['IMAGE']['LINES'],label['IMAGE']['LINE_SAMPLES'])
+
+def read_dat(filename,write_csv=False,quiet=True):
+    """ Reads a PDS4 .dat format file, preserving column order and data type,
+    except that byte order is switched to native if applicable. The .dat file
+    and .xml label must exist in the same directory.
+    """
+    if filename[-4:]=='.dat':
+        filename.replace('.dat','.xml')
+    if filename[-4:]!='.xml':
+        raise TypeError('Unknown filetype: {ext}'.format(ext=filename[-4:]))
+    structures = pds4_tools.pds4_read(filename,quiet=quiet)
+    dat_dict = OrderedDict({})
+    for i in range(len(structures[0].fields)):
+        name = structures[0].fields[i].meta_data['name']
+        dat_dtype = structures[0].fields[i].meta_data['data_type']
+        dtype = pds4_tools.reader.data_types.pds_to_numpy_type(dat_dtype)
+        data = np.array(structures[0].fields[i],dtype=dtype)
+        if ((sys.byteorder=='little' and '>' in str(dtype)) or
+            (sys.byteorder=='big' and '<' in str(dtype))):
+            data = data.byteswap().newbyteorder()
+        dat_dict[name]=data
+    dataframe = pd.DataFrame(dat_dict)
+    if write_csv:
+        dataframe.to_csv(filename.replace('.xml','.csv'),index=False)
+    return dataframe
+
+def dat_to_csv(filename):
+    """ Converts a PDS4 file to a Comma Separated Value (CSV) file with
+    the same base filename. The .dat file and .xml label must exist in
+    the same directory.
+    """
+    _ = read_dat(filename,write_csv=True)
