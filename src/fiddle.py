b00e72560804f5a2f37340e536a52c7c66bd4671
Chase Million <chase@millionconcepts.com>
2018-06-15 19:58:20 -0400

    cleaning up old code

--- src/fiddle.py
+++
@@ -1,8 +0,0 @@
-common_parameters = ['archive-status', 'identifier', 'instrument',
-    'instrument-host', 'instrument-host-type', 'instrument-type',
-    'investigation', 'observing-system', 'person', 'product-class',
-    'start-time', 'stop-time', 'target', 'target-type', 'term', 'title']
-instance_specific_parameters = ['q','ofsn']
-control_parameters = ['start', 'rows', 'return-type', 'package', 'rt']
-
-baseURL = 'http://pds.nasa.gov/services/search/search'
